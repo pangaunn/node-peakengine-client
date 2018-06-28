77e620b093efb8b92ea69a09f227284d1f720fc6
aunn <aunn_it08@hotmail.com>
2018-06-28 18:57:57 +0700

    change process.env to params

--- src/PeackEngineClient.js
+++ src/PeackEngineClient.js
@@ -1,14 +1,13 @@
 const axios = require('axios')
-
-function newClient (url, token) {
-  const peakApi = axios.create({
-    baseURL: process.env.API_PEAK_ENGINE,
-    timeout: 20000,
-    headers: {
-      'Content-Type': 'Application/json',
-      'User-Token': process.env.USER_TOKEN_PEAK
-    }
-  })
+module.exports = class PeakEngineClient {
+  constructor (url, token) {
+    this.client = axios.create({
+      baseURL: url,
+      timeout: 20000,
+      headers: {
+        'Content-Type': 'Application/json',
+        'User-Token': token
+      }
+    })
+  }
 }
-
-module.exports = newClient
