edde84362be80c603777a2e0afc8cbb660c27052
Thanakorn.K <potook_02@hotmail.com>
2018-06-29 19:36:54 +0700

    add findContact

--- src/PeackEngineClient.js
+++ src/PeackEngineClient.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const crypto = require('crypto')
 module.exports = class PeakEngineClient {
   constructor (url, token) {
     this.client = axios.create({
@@ -10,17 +11,41 @@ module.exports = class PeakEngineClient {
       }
     })
 
-    this.client.interceptors.request.use(peakEngineRequestInterceptor)
+    // this.client.interceptors.request.use(peakEngineRequestInterceptor)
+  }
+  async findContact (query, target) {
+    const { data: contacts } = await this.client.get(`/contacts?${target}=${query}`)
+    return contacts
+  }
+  getTimeStamp () {
+    return generateHeaders('ssssss')
   }
 }
 
-function peakEngineRequestInterceptor (config) {
-  const { timeStamp, encrypted } = generateHeaders()
-  config.headers['Time-Stamp'] = `${timeStamp}`
-  config.headers['Time-Signature'] = encrypted
-  return config
+// function peakEngineRequestInterceptor (config) {
+//   const { timeStamp, encrypted } = generateHeaders()
+//   config.headers['Time-Stamp'] = `${timeStamp}`
+//   config.headers['Time-Signature'] = encrypted
+//   return config
+// }
+
+function generateHeaders (CONNECTION_ID) {
+  let hmacsha1 = crypto.createHmac('sha1', CONNECTION_ID).update(timeStamp())
+  let encrypted = hmacsha1.digest('hex')
+  return {encrypted, timeStamp: timeStamp()}
 }
 
-function generateHeaders () {
+function timeStamp () {
+  const now = new Date();
+  const year = now.getUTCFullYear().toString();
+  const month = (now.getUTCMonth() + 1).toString();
+  const date = now.getUTCDate().toString();
+  const hour = now.getUTCHours().toString();
+  const minute = now.getUTCMinutes().toString();
+  const second = now.getUTCSeconds().toString();
+  return `${year}${generateTime(month)}${generateTime(date)}${generateTime(hour)}${generateTime(minute)}${generateTime(second)}`
+}
 
+function generateTime (time) {
+  return (time < 10) ? '0' + time : time
 }
