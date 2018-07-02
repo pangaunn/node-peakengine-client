2d27141f23370cb80e98d020847c56f1d4c19dc3
aunn <aunn_it08@hotmail.com>
2018-07-02 19:56:59 +0700

    use encryptWIthSha1 in generateHeaders

--- src/PeackEngineClient.js
+++ src/PeackEngineClient.js
@@ -25,7 +25,7 @@ module.exports = class PeakEngineClient {
     const timeStamp = DateTime.utc().toFormat('YYYYMMDDHHmmss')
     return {
       timeStamp: DateTime.utc().toFormat('YYYYMMDDHHmmss'),
-      encryptedTimeStamp: this.encryptTimeStamp(timeStamp)
+      encryptedTimeStamp: this.encryptWithSha1(timeStamp)
     }
   }
 
